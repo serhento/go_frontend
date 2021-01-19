d0a2645edb4b6283c6d8bd2718d07086473e612c
serhento <serhento@gmail.com>
2021-01-19 16:57:06 +0300

    init

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="v-main-wrapper">
+        <keep-alive>
+            <router-view>
+
+            </router-view>
+        </keep-alive>
+
+<!--        <vCatalog/>-->
+<!--        <vCart-->
+<!--            v-if="CART.length"-->
+<!--            :cart_data="CART"-->
+<!--        />-->
+    </div>
+</template>
+
+<script>
+    // import vCatalog from './v-catalog';
+    // import vCart from './v-cart';
+    //import {mapGetters} from 'vuex';
+    export default {
+        name: "v-main-wrapper",
+        // components:{
+        //     vCatalog, vCart
+        // },
+        data(){
+            return{
+
+            }
+        },
+        // computed:{
+        //     ...mapGetters([
+        //         'CART'
+        //     ])
+        // }
+    }
+</script>
+
+<style>
+    .v-main-wrapper{
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import vCatalog from '../components/v-catalog';
+import vCart from '../components/v-cart';
+import vCartItem from '../components/v-cart-item';
+
+Vue.use(Router);
+
+let router = new Router({
+    routes:[
+        {
+            path: '/',
+            name: 'catalog',
+            component: vCatalog
+        },
+        {
+            path: '/cart',
+            name: 'cart',
+            component: vCart,
+            props: true
+        },
+        {
+            path: '/catalog-item',
+            name: 'cart-item',
+            component: vCartItem,
+            props: true
+        }
+    ]
+});
+
+export default router;
